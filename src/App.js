2d6edd661f48cf37e45ef4ec49e4b3c48a15908e
sahmad <sahmad23@g.ucla.edu>
2022-10-16 23:15:35 -0700

    added interview boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import DataCard from './components/DataCard'
+
+
+// Let's assume you received this data from one of our internal APIs. 
+// You want to display all of the data on the telemetry page for our operators!
+const DATA = [
+  {run_title: "Iteration #1", x: 0.12, y: 0.78, z: 0.91},
+  {run_title: "Iteration #2", x: 0.14, y: 0.76, z: 0.09},
+  {run_title: "Iteration #3", x: 0.18, y: 0.72, z: 0.11},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div> Use this as a sanity check to make sure you can see something. </div>
+      <div className="App">
+        {/** 
+         * TODO: We want to iterate over the data in each card and list it out on the page
+        */}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/DataCard.js
@@ -0,0 +1,14 @@
+/**
+ * TODO:
+ * 1) The card component should take in props. Display all the data within the card
+ * 2) For whatever reason, we want to let users click on the card and also keep track of how many times we did so!
+ *    So, we should also create some sort of logic to keep track of how many times someone clicks on a card!
+ * 
+ * NOTE: It's fine if the styling isn't perfect bc who knows CSS off the top of their head anyways
+ */
+
+export const DataCard = () => {
+    return(
+        <div> you should probably remove this </div>
+    )
+}
